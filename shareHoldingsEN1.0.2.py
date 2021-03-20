a371fcb88b97c61d28944b3f4783226134b35682
Eric Li <ericlxhk@gmail.com>
2021-03-20 19:16:58 +0800

    Update shareHoldingsEN1.0.2.py

--- shareHoldingsEN1.0.2.py
+++ shareHoldingsEN1.0.2.py
@@ -1,15 +1,15 @@
 # Project Name: ShareholdingEN
 # Version: 1.0.2
 # Updated: Eric
-# Date: 17 March, 2021
+# Date: 20 March, 2021
 
-# Standard library for Python
+# Standard library from Python
 import re
 import sys
 import datetime
 from time import sleep
 
-# Library need to be installed
+# Library to be installed
 import xlrd
 import xlwt
 import requests
@@ -32,6 +32,13 @@ def get_every_day(begin_date, end_date):
         dateList.append(date_str)
         beginDate += datetime.timedelta(days=1)
     return dateList
+
+# To extract data required with regular expresseion
+def re_extract(reg, html):
+    page = html.replace('\n', '').replace('\r', '')
+    results = re.findall(reg, page)
+    return results
+
 
 # To get the formdata required for post requests
 def get_form_data(url, headers, search_date):
@@ -72,80 +79,85 @@ def get_headers():
         'user-agent': header_content
     }
 
-sp = int(input('Enter 0 for shares, enter 1 for percentage: '))
-selection = 5
-if sp == 1: selection = 7
-request_list = ['Share', 'Percentage']
-start, end, datelist = obtain_date()
-number_of_days = 0
-title = ['Stock code', 'Stock Name']
-output = {}
+# Main program
+if __name__ == '__main__':
+    sp = int(input('Enter 0 for shares, enter 1 for percentage: '))
+    selection = 5
+    if sp == 1:
+        selection = 7
 
-url = 'https://www.hkexnews.hk/sdw/search/mutualmarket.aspx?t=hk'
-headers = get_headers()
+    start, end, datelist = obtain_date()
+    number_of_days = 0
+    output = {}
 
-repl1, repl2, repl3 = ' ' * 40, ' ' * 44, ' ' * 48
-reg = f'''\
-<tr>\
-{repl2}<td class="col-stock-code">\
-{repl3}<div class="mobile-list-heading">(.*?)</div>\
-{repl3}<div class="mobile-list-body">(.*?)</div>\
-{repl2}</td>\
-{repl2}<td class="col-stock-name">\
-{repl3}<div class="mobile-list-heading">(.*?)</div>\
-{repl3}<div class="mobile-list-body">(.*?)</div>\
-{repl2}</td>\
-{repl2}<td class="col-shareholding">\
-{repl3}<div class="mobile-list-heading">(.*?)</div>\
-{repl3}<div class="mobile-list-body">(.*?)</div>\
-{repl2}</td>\
-{repl2}<td class="col-shareholding-percent">\
-{repl3}<div class="mobile-list-heading">(.*?)</div>\
-{repl3}<div class="mobile-list-body">(.*?)</div>\
-{repl2}</td>\
-{repl1}</tr>\
-'''
+    request_list = ['Share', 'Percentage']
+    title = ['Stock code', 'Stock Name']
 
-for date in datelist:
-    search_date = '{}/{}/{}'.format(date[:4], date[4:6], date[6:])
-    try:
-        title.append(search_date)
-        content = get_content(url, get_form_data(url, headers, search_date))
-        page = content.replace('\n', '').replace('\r', '')
-        results = re.findall(reg, page)
-        for item in results:
-            if output.get(int(item[1])):
-                output[int(item[1])].append(item[selection])
-            else:
-                output[int(item[1])] = []
-                output[int(item[1])].append(item[3])
-                for i in range(0, number_of_days):
-                    output[int(item[1])].append('N/A')
-                output[int(item[1])].append(item[selection])
-        print('{} Done!'.format(search_date))
-        number_of_days += 1
-    except:
-        print('Error: {} is not available!'.format(search_date))
-    sleep(3)
+    url = 'https://www.hkexnews.hk/sdw/search/mutualmarket.aspx?t=hk'
+    headers = get_headers()
 
-f = xlwt.Workbook(encoding='utf-8', style_compression=0)
-sheet = f.add_sheet('Shareholdings_{}'.format(request_list[sp]))
+    repl1, repl2, repl3 = ' ' * 40, ' ' * 44, ' ' * 48
+    reg = f'''\
+    <tr>\
+    {repl2}<td class="col-stock-code">\
+    {repl3}<div class="mobile-list-heading">(.*?)</div>\
+    {repl3}<div class="mobile-list-body">(.*?)</div>\
+    {repl2}</td>\
+    {repl2}<td class="col-stock-name">\
+    {repl3}<div class="mobile-list-heading">(.*?)</div>\
+    {repl3}<div class="mobile-list-body">(.*?)</div>\
+    {repl2}</td>\
+    {repl2}<td class="col-shareholding">\
+    {repl3}<div class="mobile-list-heading">(.*?)</div>\
+    {repl3}<div class="mobile-list-body">(.*?)</div>\
+    {repl2}</td>\
+    {repl2}<td class="col-shareholding-percent">\
+    {repl3}<div class="mobile-list-heading">(.*?)</div>\
+    {repl3}<div class="mobile-list-body">(.*?)</div>\
+    {repl2}</td>\
+    {repl1}</tr>\
+    '''
 
-for index, cell in enumerate(title):
-    sheet.write(0, index, cell)
-to_excel = []
-for key, value in output.items():
-    temp = []
-    temp.append(key)
-    temp.extend(value)
-    to_excel.append(temp)
-for i, j in enumerate(to_excel):
-    for m, n in enumerate(j):
-        sheet.write(i + 1, m, n)
+    for date in datelist:
+        search_date = '{}/{}/{}'.format(date[:4], date[4:6], date[6:])
+        sleep(3)
+        try:
+            title.append(search_date)
+            content = get_content(url, get_form_data(url, headers, search_date))
+            results = re_extract(reg, content)
+            for item in results:
+                if output.get(int(item[1])):
+                    output[int(item[1])].append(item[selection])
+                else:
+                    output[int(item[1])] = []
+                    output[int(item[1])].append(item[3])
+                    for i in range(0, number_of_days):
+                        output[int(item[1])].append('N/A')
+                    output[int(item[1])].append(item[selection])
+            print('{} Done!'.format(search_date))
+            number_of_days += 1
+        except:
+            print('Error: {} is not available!'.format(search_date))
 
-date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-file_saved = 'ShareholdingEN_{}_{}_at_{}.xls'.format(start, end, date_time)
-file_path = sys.path[0] + '\\' + file_saved
-f.save(file_path)
+    to_excel = []
+    for key, value in output.items():
+        temp = []
+        temp.append(key)
+        temp.extend(value)
+        to_excel.append(temp)
 
-exiting = input('Completed! Press [Enter] to exit.')
+    f = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet = f.add_sheet('Shareholdings_{}'.format(request_list[sp]))
+    for index, cell in enumerate(title):
+        sheet.write(0, index, cell)
+
+    for i, j in enumerate(to_excel):
+        for m, n in enumerate(j):
+            sheet.write(i + 1, m, n)
+
+    date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+    file_saved = 'ShareholdingEN_{}_{}_at_{}.xls'.format(start, end, date_time)
+    file_path = sys.path[0] + '\\' + file_saved
+    f.save(file_path)
+
+    exiting = input('Completed! Press [Enter] to exit.')
